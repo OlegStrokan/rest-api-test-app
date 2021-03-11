32d8f7e4f383fe526f41da7b24034b1fee185df2
olegstrokan <oleg14ua71@gmail.com>
2021-03-11 13:26:12 +0100

    Version 4.5. Beta 4. Add lazy loading for comments page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css'
 import {Route} from "react-router-dom";
 import MyData from "./components/MyData/MyData";
 import MyPostsContainer from "./components/MyPosts/MyPostsContainer";
-import MyCommentsContainer from "./components/MyComments/MyCommentsContainer";
 import MyAlbumsContainer from "./components/MyAlbums/MyAlbumsContainer";
 import MyPhotosContainer from "./components/MyPhotos/MyPhotosContainer";
 import MyTodosContainer from "./components/MyTodos/MyTodosContainer";
@@ -11,23 +10,27 @@ import MyUsersContainer from "./components/MyUsers/MyUsersContainer";
 import Start from "./components/Start/Start";
 import LoginContainer from "./components/Login/LoginContainer";
 
+const MyCommentsContainer = React.lazy(() => import('./components/MyComments/MyCommentsContainer'))
 
 function App() {
     return <div className="container">
         <div className={"navbar"}>
-           <MyData/>
+            <MyData/>
         </div>
-           <div className="wrapper">
-               <Route path={'/posts'} render={() => <MyPostsContainer/>}/>
-               <Route path={'/comments'} render={() => <MyCommentsContainer/>}/>
-               <Route path={'/albums'} render={() => <MyAlbumsContainer/>}/>
-               <Route path={'/photos'} render={() => <MyPhotosContainer/>}/>
-               <Route path={'/todos'} render={() => <MyTodosContainer/>}/>
-               <Route path={'/users'} render={() => <MyUsersContainer/>}/>
-               <Route path={'/login'} render={() => <LoginContainer/>}/>
-               <Route exact path={'/'} render={() => <Start/>}/>
+        <div className="wrapper">
+            <Route path={'/posts'} render={() => <MyPostsContainer/>}/>
+            <Route path={'/comments'} render={() =>
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    <MyCommentsContainer/>
+                </React.Suspense>}/>
+            <Route path={'/albums'} render={() => <MyAlbumsContainer/>}/>
+            <Route path={'/photos'} render={() => <MyPhotosContainer/>}/>
+            <Route path={'/todos'} render={() => <MyTodosContainer/>}/>
+            <Route path={'/users'} render={() => <MyUsersContainer/>}/>
+            <Route path={'/login'} render={() => <LoginContainer/>}/>
+            <Route exact path={'/'} render={() => <Start/>}/>
 
-           </div>
+        </div>
     </div>
 }
 
